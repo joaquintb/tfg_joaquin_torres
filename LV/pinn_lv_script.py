c1d770c0b35f6646d4705bea45f7b80c49f5ce37
joaquintb <joaquintobrw@gmail.com>
2024-05-28 21:05:06 +0200

    Script for LV ready

--- LV/pinn_lv_script.py
+++ LV/pinn_lv_script.py
@@ -1,31 +1,32 @@
+# Author: Joaquín Torres Bravo
+# Date: June 2024
+# Bachelor Thesis
+
 # Libraries
 import numpy as np
 import matplotlib.pyplot as plt
 import torch
 import torch.nn as nn
 import tqdm as tqdm
-import time
 
-# Solving the system
-def LV(x,a,b,c,d):    
-    xdot = np.array([a*x[0] - b*x[0]*x[1], d*x[0]*x[1] - c*x[1]])
-    return xdot
-def RK4(f, x0, t0, tf, dt):
-    t = np.arange(t0, tf, dt)
+# RK4 to numerically solve the LV system (Vanessa)
+def RK4(f, x0, t0, tf, num_points):
+    t = np.linspace(t0,tf, num_points)
+    dt = (tf - t0) / (num_points - 1)
     nt = t.size
     nx = x0.size
     x = np.zeros((nx,nt))
-    x[:,0] = x0
+    x[:,0] = x0 # Initial condition
     for k in range(nt-1):
         k1 = dt*f(t[k], x[:,k])
         k2 = dt*f(t[k] + dt/2, x[:,k] + k1/2)
         k3 = dt*f(t[k] + dt/2, x[:,k] + k2/2)
         k4 = dt*f(t[k] + dt, x[:,k] + k3)
         dx=(k1 + 2*k2 + 2*k3 +k4)/6
-        x[:,k+1] = x[:,k] + dx;  
+        x[:,k+1] = x[:,k] + dx
     return x, t
 
-# Defining the network
+# PINN model definition
 class FCN(nn.Module):
     "Defines a standard fully-connected network in PyTorch with sinusoidal activation"
     def __init__(self, N_INPUT, N_OUTPUT, N_HIDDEN, N_LAYERS):
@@ -40,7 +41,6 @@ class FCN(nn.Module):
                             activation()]) for _ in range(N_LAYERS-1)])
         self.fce_predator = nn.Linear(N_HIDDEN, N_OUTPUT)
         self.fce_prey = nn.Linear(N_HIDDEN, N_OUTPUT)
-
     def forward(self, x):
         x = self.fcs(x)
         x = self.fch(x)
@@ -48,94 +48,95 @@ class FCN(nn.Module):
         predator_output = self.fce_predator(x)
         return prey_output, predator_output
 
-# Training function
-def train_pinn(pinn, optimizer, t_physics, t_obs, u_obs_x, u_obs_y, target_params, tolerance, lambda1=1e5):
-    
-    a = torch.nn.Parameter(torch.empty(1).uniform_(0, 10), requires_grad=True)
-    b = torch.nn.Parameter(torch.empty(1).uniform_(0, 10), requires_grad=True)
-    c = torch.nn.Parameter(torch.empty(1).uniform_(0, 10), requires_grad=True)
-    d = torch.nn.Parameter(torch.empty(1).uniform_(0, 10), requires_grad=True)
-
-    optimizer.add_param_group({'params': [a, b, c, d]})
-
-    target_a, target_b, target_c, target_d = target_params
-    start_time = time.time()
-    
-    for i in tqdm.tqdm(range(20000)):
-        optimizer.zero_grad()
-
-        u1, u2 = pinn(t_physics)
-        du1dt = torch.autograd.grad(u1, t_physics, torch.ones_like(u1), create_graph=True)[0]
-        du2dt = torch.autograd.grad(u2, t_physics, torch.ones_like(u2), create_graph=True)[0]
-        phy_loss_x = torch.mean((du1dt - a*u1 + b*u1*u2) ** 2)
-        phy_loss_y = torch.mean((du2dt + c*u2 - d*u1*u2) ** 2)
-        total_physics_loss = phy_loss_x + phy_loss_y
-
-        u1, u2 = pinn(t_obs)
-        data_loss_x = torch.mean((u1 - u_obs_x) ** 2)
-        data_loss_y = torch.mean((u2 - u_obs_y) ** 2)
-        total_data_loss = data_loss_x + data_loss_y
-
-        loss = total_physics_loss + lambda1 * total_data_loss
-        loss.backward()
-        optimizer.step()
-
-        if (abs(a.item() - target_a) < tolerance and 
-            abs(b.item() - target_b) < tolerance and 
-            abs(c.item() - target_c) < tolerance and 
-            abs(d.item() - target_d) < tolerance):
-            break
-
-    execution_time = time.time() - start_time
-    return execution_time, i, (a.item(), b.item(), c.item(), d.item())
-
-if __name__ == "__main__":
+if __name__=="__main__": 
+    # Setup
     # Defining the problem
-    a = 1
-    b = 1
-    c = 1
-    d = 1
-    f= lambda t,x : LV(x,a,b,c,d)         # lambda is an anonymous function which can take may inputs but returns one output. Same case is with MATLAB denoted by @.
-    x0 = np.array([0.5,1])                # initial condition    
+    a,b,c,d = 1,1,1,1
+    f=lambda t, x: np.array([a*x[0] - b*x[0]*x[1], d*x[0]*x[1] - c*x[1]])
+    x0 = np.array([0.5,1]) # IC
     # Solving the problem
-    t0 = 0                                # time unit is second
-    tf = 15
+    t0, tf = 0, 15
     num_points = 1000
-    dt = (tf - t0) / (num_points - 1)
-    x, t = RK4(f, x0, t0, tf, dt)
-    times =[1.1, 2.4, 3.9, 5.6, 7.5, 9.6, 11.9, 14.4]
-    for time in times:
-        print(time in t)
-    
-    exit()
-    # Generating obs data
-    random_indices = np.random.choice(len(t), size=100, replace=False)
-    x_obs = [x[0][ind] for ind in random_indices]
-    y_obs = [x[1][ind] for ind in random_indices]
-    # Generate Gaussian noise for x_obs and y_obs
+    x, t = RK4(f, x0, t0, tf, num_points)
+
+    # Generating observations
+    # Generate 8 equispaced time locations in the domain for fair comparison with ABC-SMC
+    obs_ind = np.linspace(0, len(t) - 1, 8, dtype=int)
+    # Get solution at those time locations
+    x_obs = [x[0][ind] for ind in obs_ind]
+    y_obs = [x[1][ind] for ind in obs_ind]
+    # Generate Gaussian noise to add to the observations
     noise_x = 0.03 * np.random.randn(len(x_obs))
     noise_y = 0.03 * np.random.randn(len(y_obs))
     # Add the noise to the observed values
     x_obs_noise = [x_obs[ind] + noise_x[ind] for ind in range(len(x_obs))]
     y_obs_noise = [y_obs[ind] + noise_y[ind] for ind in range(len(y_obs))]
+    # Tensor containing time locations for observations
+    t_obs = torch.tensor([t[ind] for ind in obs_ind], dtype=torch.float32).view(-1,1)
+    # Tensor with noisy observations for prey
+    u_obs_x = torch.tensor(x_obs_noise,  dtype=torch.float32).view(-1,1)
+    # Tensor with noisy observations for predator
+    u_obs_y = torch.tensor(y_obs_noise,  dtype=torch.float32).view(-1,1)
 
-    # Tensors
-    t_obs = torch.tensor([t[ind] for ind in random_indices], dtype=torch.float32).view(-1,1)
-    u_obs_x = torch.tensor(x_obs_noise,  dtype=torch.float32).view(-1,1) # Noise?
-    u_obs_y = torch.tensor(y_obs_noise,  dtype=torch.float32).view(-1,1) 
-    t_physics = []
-    for i in range(1000):
-        if i % 20 == 0:
-            t_physics.append(t[i])
-    t_physics = torch.tensor(t_physics, dtype=torch.float32).view(-1,1).requires_grad_(True)
+    # Physic loss training points
+    # Tensor of times to train PINN through physics loss
+    t_physics = torch.linspace(t0, tf, 500).view(-1, 1).requires_grad_(True)
+    # Tensor of times to check progress of PINN when training
+    t_test = torch.linspace(t0, tf, 200).view(-1, 1).requires_grad_(True)
 
-    # Initializing 
-    pinn = FCN(1,1,32,3)
-    optimizer = torch.optim.Adam(pinn.parameters(), lr=1e-3)
-    target_params = (1.0, 1.0, 1.0, 1.0)
-    tolerance = 0.02
+    # Training setup
+    # Initialize PINN
+    pinn = FCN(1,1,32,3) 
+    # Sample initial problems parameters from uniform 0 to 10 for comparison to be fair
+    a = torch.nn.Parameter(torch.empty(1).uniform_(0, 10), requires_grad=True)
+    b = torch.nn.Parameter(torch.empty(1).uniform_(0, 10), requires_grad=True)
+    c = torch.nn.Parameter(torch.empty(1).uniform_(0, 10), requires_grad=True)
+    d = torch.nn.Parameter(torch.empty(1).uniform_(0, 10), requires_grad=True)
+    # Define optimiser 
+    optimiser = torch.optim.Adam(list(pinn.parameters())+[a,b,c,d],lr=1e-3)
+    # Hyperparameter deciding weight given to fitting observational data
+    lambda_weight = 100
+    # Number of training iterations
+    max_it = 10000
 
-    exec_time, end_it, final_params = train_pinn(pinn, optimizer, t_physics, t_obs, u_obs_x, u_obs_y, target_params, tolerance)
-    print(f"Execution Time: {exec_time} seconds")
-    print(f"Took {end_it} iterations")
-    print(f"Final Parameters: {final_params}")
+    # Training loop
+    for i in tqdm.tqdm(range(max_it)):
+        # Reset gradient to zero
+        optimiser.zero_grad()
+
+        # -----------------------
+        #       PHYSICS LOSS
+        # -----------------------
+        u1,u2 = pinn(t_physics)
+        # Compute the derivatives with respect to time
+        du1dt = torch.autograd.grad(u1, t_physics, torch.ones_like(u1), create_graph=True)[0]
+        du2dt = torch.autograd.grad(u2, t_physics, torch.ones_like(u2), create_graph=True)[0]
+        # Compute the physics loss for Lotka-Volterra equations
+        phy_loss_x = torch.mean((du1dt - a*u1 + b*u1*u2) ** 2)
+        phy_loss_y = torch.mean((du2dt + c*u2 - d*u1*u2) ** 2)
+        # Compute total physics loss
+        total_physics_loss = phy_loss_x + phy_loss_y
+
+        # -----------------------
+        #       DATA LOSS
+        # -----------------------
+        # Compute the PINN output
+        u1, u2 = pinn(t_obs)
+        # Compute the data loss for the first equation (x)
+        data_loss_x = torch.mean((u1 - u_obs_x)**2)
+        # Compute the data loss for the second equation (y)
+        data_loss_y = torch.mean((u2 - u_obs_y)**2)
+        # Compute total data loss
+        total_data_loss = data_loss_x + data_loss_y
+
+        # Compute total loss
+        loss = total_physics_loss + lambda_weight*total_data_loss
+
+        # Backpropagate joint loss, take optimiser step
+        loss.backward()
+        optimiser.step()
+           
+    print(a.item())
+    print(b.item())
+    print(c.item())
+    print(d.item())
